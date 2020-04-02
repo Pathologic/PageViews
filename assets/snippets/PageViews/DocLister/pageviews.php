c4daa88a0bd261aa52b372f7236266485dc16e24
Pathologic <m@xim.name>
2020-04-02 13:25:14 +0300

    fix

--- assets/snippets/PageViews/DocLister/pageviews.php
+++ assets/snippets/PageViews/DocLister/pageviews.php
@@ -15,9 +15,9 @@ class pageviewsDocLister extends site_contentDocLister
     public static function prepare(array $data = array(), DocumentParser $modx, $_DocLister, prepare_DL_Extender $_extDocLister){
         $unit = $_DocLister->getCFGDef('unit','просмотр,просмотра,просмотров');
         $unit = explode(',',$unit);
-        if (isset($data['views']) && count($unit>2)) {
+        if (isset($data['views']) && count($unit) > 2) {
             $cases = [2, 0, 1, 1, 1, 2];
-            $data['unit'] = sprintf($unit[ ($data['views']%100>4 && $data['views']%100<20) ? 2 : $cases[min($data['views']%10, 5)] ], $data['views']);
+            $data['unit'] = sprintf($unit[ ($data['views'] % 100 > 4 && $data['views'] % 100 < 20) ? 2 : $cases[min($data['views'] % 10, 5)] ], $data['views']);
         }
 
         return $data;
